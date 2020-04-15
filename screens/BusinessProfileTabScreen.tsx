332c604754d900360bce67770e9cb7fb6541b705
jirwincline <jci@pdx.edu>
2020-04-14 22:04:56 -0700

    add tab screens

---
+++ screens/BusinessProfileTabScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const BusinessProfileTabScreen = (props) => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default BusinessProfileTabScreen;
+
+const styles = StyleSheet.create({});
