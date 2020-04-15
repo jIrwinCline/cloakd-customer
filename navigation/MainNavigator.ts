c589ceaa1864f48eb79074d4fce3fbf68917130b
jirwincline <jci@pdx.edu>
2020-04-14 23:02:26 -0700

    add main app navigator that is used in app.tsx

---
+++ navigation/MainNavigator.ts
@@ -0,0 +1,31 @@
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+// import LoginScreen from "../screens/LoginScreen";
+
+import Colors from "../constants/colors";
+
+import BottomTabNavigator from "./BottomTabNavigator";
+
+// const AuthStack = createStackNavigator({ LoginSignup: LoginSignupScreen });
+// const MessageStack = createStackNavigator({ Message: MessageScreen });
+
+const MainNavigator = createSwitchNavigator(
+  {
+    // Auth: AuthStack,
+    Tabs: BottomTabNavigator,
+    // Message: MessageStack,
+  },
+  {
+    initialRouteName: "Tabs",
+  }
+  // {
+  //   defaultNavigationOptions: {
+  //     headerStyle: {
+  //       backgroundColor: Colors.primary
+  //     },
+  //     headerTintColor: Platform.OS === "android" ? "white" : "white"
+  //     //   headerShown: false
+  //   }
+  // }
+);
+export default createAppContainer(MainNavigator);
